984877ca02ba4958e44b0e1496461d1b1f264b22
Cain Atkinson <yellowsink@protonmail.com>
2021-10-30 11:19:28 +0100

    i think this works?

--- index.jsx
+++ index.jsx
@@ -1,13 +1,26 @@
 import { createResource, createSignal } from "solid-js";
+import { Remarkable } from "remarkable";
 
-export default ({ children, src }) => {
+export default ({ children, src, css, loading }) => {
   const getMarkdownSource = async () => await (await fetch(src)).text();
 
   const [fetchSignal, setFetchSignal] = createSignal(false);
   let [markdown] = src
     ? createResource(fetchSignal, getMarkdownSource)
-    : [children];
+    : [() => children];
 
-		
-  return <div />;
+  const md = new Remarkable();
+
+  const LoadingComponent = () =>
+    typeof loading === "string" ? <div class="solidown-loading">{loading}</div> : <loading />;
+
+  return (
+    <div class="solidown-wrapper">
+      <div class={`solidown-markdown-root`}>
+        {markdown() ? md.render(markdown()) : <LoadingComponent />}
+      </div>
+
+      {css ? <style>{css}</style> : []}
+    </div>
+  );
 };
